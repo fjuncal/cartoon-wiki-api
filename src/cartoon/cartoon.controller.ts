82c06e270ef9689c37bca743a4545c18e6923934
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-15 23:02:42 -0300

    Adicionando metodo de obterTodos() no cartoon controller

--- src/cartoon/cartoon.controller.ts
+++ src/cartoon/cartoon.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import Cartoon from './cartoon.entity';
 import { CartoonRepository } from './cartoon.repository';
 
@@ -10,5 +10,10 @@ export class CartoonController {
   async criar(@Body() cartoon: Cartoon) {
     const novoCartoon = await this.cartoonRepository.criar(cartoon);
     return novoCartoon;
+  }
+
+  @Get()
+  async obterTodos() {
+    return await this.cartoonRepository.obterTodos();
   }
 }
