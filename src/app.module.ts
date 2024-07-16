a08312c338ce3f68d3c999580e7cc2dc777e4aed
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-15 22:10:24 -0300

    Creating migration with prisma and cartoon module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+import { CartoonModule } from './cartoon/cartoon.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsuarioModule, CartoonModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
