0a00cd538e0092596a29d25f67015b4ef1c759e3
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-15 22:49:15 -0300

    Criando cartoon repositorio

--- src/cartoon/cartoon.module.ts
+++ src/cartoon/cartoon.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CartoonController } from './cartoon.controller';
+import { DbModule } from 'src/db/db.module';
+import { CartoonRepository } from './cartoon.repository';
 
 @Module({
+  imports: [DbModule],
   controllers: [CartoonController],
+  providers: [CartoonRepository],
 })
 export class CartoonModule {}
