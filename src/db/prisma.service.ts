9b246d36595ac30c6b5284dee4212bf74f172ee1
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-15 22:46:33 -0300

    Criando serviço do prisma e adicionando o servico no db.module

--- src/db/prisma.service.ts
+++ src/db/prisma.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Global,
+  Injectable,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+@Global()
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  onModuleDestroy() {
+    throw new Error('Method not implemented.');
+  }
+  onModuleInit() {
+    throw new Error('Method not implemented.');
+  }
+}
