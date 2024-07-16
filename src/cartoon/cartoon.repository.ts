ee95cdde1996577c962b371f4bb80c2a91b04518
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-15 22:54:09 -0300

    Implementando metodos do cartoon repository

--- src/cartoon/cartoon.repository.ts
+++ src/cartoon/cartoon.repository.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/db/prisma.service';
+import Cartoon from './cartoon.entity';
 
 @Injectable()
-export class CartoonRepository {}
+export class CartoonRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async obterTodos() {
+    return await this.prismaService.cartoon.findMany();
+  }
+
+  async obterPorId(id: number) {
+    return await this.prismaService.cartoon.findUnique({ where: { id } });
+  }
+
+  async criar(cartoon: Cartoon) {
+    return await this.prismaService.cartoon.create({ data: cartoon as any });
+  }
+
+  async atualizar(cartoon: Cartoon) {
+    if (!cartoon.id) return null;
+    return await this.prismaService.cartoon.update({
+      where: { id: cartoon.id },
+      data: cartoon as any,
+    });
+  }
+}
