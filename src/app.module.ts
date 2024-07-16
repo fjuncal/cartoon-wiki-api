01a0fac7190a9c157c9f25ac253dc7f3b27858de
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-15 22:43:47 -0300

    Criando db module e prisma service para acessar banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CartoonModule } from './cartoon/cartoon.module';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [CartoonModule],
+  imports: [CartoonModule, DbModule],
   controllers: [],
   providers: [],
 })
