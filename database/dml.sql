47d07b7fde424895e0b05a9e0819225f97f989e3
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-20 00:59:35 -0300

    Comitando os inserts na tabela

--- database/dml.sql
+++ database/dml.sql
@@ -1,3 +1,71 @@
 INSERT INTO public."Cartoon"(
 	id, nome, descricao, foto)
 	VALUES (?, ?, ?, ?);
+
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Piccolo", "Piccolo is the son/reincarnation of the Namekian Piccolo Daimaō, and was spawned to avenge his sire's death at the hands of Goku. As time went on, he abandoned his ambitions to rule Earth and eventually joined Goku and the Z-Fighters as an ally and a friend. He seems to also share a deep bond with Goku's son Gohan, and appears to be a sort of second father to him. Piccolo has many signature moves such as the Special Beam Cannon, Hellzone Grenade, and Light Grenade.", "https://comicvine.gamespot.com/a/uploads/scale_small/0/6130/151422-10016-piccolo.jpg");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Gohan", "The first son of Goku and Chichi and a half-Human, half-Saiyan, Son Gohan held the potential for enormous power. Growing up a scholar, not a warrior like his father, he nevertheless has answered the call to protect Earth multiple times. He is the most powerful non fused warrior in the Dragon Ball Z universe by the end of Dragon Ball Z series. He is the father of Pan and husband to Videl. He is Goten's older brother and Grandson to both Ox King and Bardock, respectively. (Goku and Chichi's fathers)", "https://comicvine.gamespot.com/a/uploads/scale_small/8/80111/3365648-gohan_by_feeh05051995-d5a712v.png");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Buu", "After the evil separated from Majin Buu, the good Buu remained. He would stay a great friend to Hercule and an ally to the Z-Fighters.", "https://comicvine.gamespot.com/a/uploads/scale_small/0/5078/141684-187603-buu.jpg");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Bulma Briefs",  "Bulma's family runs the Capsule Corporation. Bulma is the friend of Goku and Krillin. She is a scientist like her father. She is married to Vegeta. They have two children.", "https://comicvine.gamespot.com/a/uploads/scale_small/3/38919/5119633-bulma%20-%20dragon%20ball%20super.jpg");
+
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Krillin",  "Krillin is the most powerful human in the Dragonball Universe. Krillin is short, bald and has six dots tattooed on his forehead. He usually wears the the turtle hermit school name on his uniform. Krillin is married and has a daughter. After the cell games, Krillin eventually grew hair.", "https://comicvine.gamespot.com/a/uploads/scale_small/1/14431/5732493-2944624772-4014b.png");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Android 17",  "Android #17 was a creation of Dr. Gero in his quest to destroy Goku. He is Android #18's twin brother. After the events of the Cell Game he spent his life traveling around the world.", "https://comicvine.gamespot.com/a/uploads/scale_small/3/39241/790748-main.jpg");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Tien",  "Tien Shinhan was once an enemy of Goku in the original Dragon Ball series, but he has since reformed and would go on to become a valuable asset of the Z-Fighters in their many battles against villains that were threatening the safety and peace of Earth.", "https://comicvine.gamespot.com/a/uploads/scale_small/1/14431/5732779-3127427154--Tien.jpg");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Vegeta",  "Vegeta, formerly the Prince of all Saiyans, was previously a supervillain who later evolved into an anti-hero and eventually a hero through the duration of the DragonBall franchise. An exceptional fighting prodigy, Vegeta has trained himself since a young age; and has always had natural talents in martial arts and chi control. He is among the last surviving pure Saiyans from the planet Vegeta. Vegeta's obsession to surpass Goku is among the key traits to his character.", "https://comicvine.gamespot.com/a/uploads/scale_small/11112/111122896/5192096-vegeta_fukkatsu_no_f_2_by_bardocksonic-d9ik5c3.png");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Jaco",  "Jaco is an elite police officer in the Galactic Patrol that came to Earth to stop a galactic criminal when he crash landed on Earth.", "https://comicvine.gamespot.com/a/uploads/scale_small/6/67663/4313120-jaco.jpg");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Goten",  "Son Goten (孫悟天, Son Goten), or simply Goten in the FUNimation dub, is a fictional character in the manga DragonBall and the anime DragonBall Z and DragonBall GT. Son Goten is the second son of Son Goku and Chi Chi and is Son Gohan's little brother. He is also uncle to Pan. He became a Super Saiyan at the young age of 7, making him the youngest Super Saiyan ever.", "https://comicvine.gamespot.com/a/uploads/scale_small/1/14431/5735888-7923614686-816f7.jpg")
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Android 18",  "Android #18 was a cybernetically enhanced by Dr. Gero to aid in his quest to destroy Goku. After the Cell Games she joined the Z-Fighters and married Krillin. She has a daughter named Marron.", "https://comicvine.gamespot.com/a/uploads/scale_small/0/7475/168490-8614-android-18.gif");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Pilaf",  "A little blue guy who wants to take over the world. He is aided by Mai and Shuu.", "https://comicvine.gamespot.com/a/uploads/scale_small/1/14431/5732489-5303617731-emper.jpg");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Goku",  "The main protagonist and hero of the Dragon Ball manga series and animated television series created by Akira Toriyama. He is one of the survivors of the extinct Saiyan race. Sent as a baby to planet Earth in order to destroy it. When he arrived he was a violent kid, due to his warrior nature. However, he suffered an accident which made him lose his memory. He became a kind and calm kid. Trained, he became a talented martial artist and world's greatest defender.", "https://comicvine.gamespot.com/a/uploads/scale_small/3/39241/790730-konachan.com___41666_dragonball_son_goku.png");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Hercule",  "Mr. Satan is the World Martial Arts Champion. His daughter is videl and by extension, his granddaughter is Pan.", "https://comicvine.gamespot.com/a/uploads/scale_small/3/39241/1065238-untitled_1.png");
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Master Roshi",  "The famous Master Roshi was sought out by a young Goku, and after some... Persuading... He agreed to teach the boy.", "https://comicvine.gamespot.com/a/uploads/scale_small/1/14431/5732451-8397948238-maste.png");
+
+
+INSERT INTO public."Cartoon"(
+	 nome, descricao, foto)
+	VALUES ("Majin Buu",  "An entity as old as the universe itself, Majin Buu has spent his time between periods of chaotic destruction and slumber. Used by Bibidi, who grew to fear the uncontrollable monster, after attacking the Supreme Kais, (absorbing two, killing one,) he was rendered docile enough to be sealed and sent to planet Earth. Years later, Bibidi's clone, Babidi unleashes the fearsome creature on the universe once again.", "https://comicvine.gamespot.com/a/uploads/scale_small/3/37889/1404911-266661_untitled___m____mnn.jpg");
+
