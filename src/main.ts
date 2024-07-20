624597ac7332c9c34845e249d50ad958cfee0cbe
Fellipe Juncal <fjuncal7@gmail.com>
2024-07-20 01:51:26 -0300

    acertando cors da api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: ['http://localhost:3000/', 'https://cartoon-wiki.vercel.app/'],
+
+    credentials: true,
+
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  });
   await app
     .listen(process.env.PORT ? Number(process.env.PORT) : 3000, '0.0.0.0')
     .then(() => console.log('App running on port 3000'));
